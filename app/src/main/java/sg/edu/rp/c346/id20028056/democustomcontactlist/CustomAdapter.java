d93330cf865ecc31d107e395de85efc2dc2db850
Yati2 <20028056@myrp.edu.sg>
2022-07-28 13:39:07 +0800

    done

--- app/src/main/java/sg/edu/rp/c346/id20028056/democustomcontactlist/CustomAdapter.java
+++ app/src/main/java/sg/edu/rp/c346/id20028056/democustomcontactlist/CustomAdapter.java
@@ -12,6 +12,8 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.squareup.picasso.Picasso;
+
 import java.util.ArrayList;
 
 public class CustomAdapter extends ArrayAdapter {
@@ -19,6 +21,7 @@ public class CustomAdapter extends ArrayAdapter {
     Context parent_context;
     int layout_id;
     ArrayList<Contact> contactList;
+
 
     public CustomAdapter(@NonNull Context context, int resource, ArrayList<Contact> objects) {
         super(context, resource, objects);
@@ -39,15 +42,21 @@ public class CustomAdapter extends ArrayAdapter {
         TextView tvCode=rowView.findViewById(R.id.textViewCountryCode);
         TextView tvNum=rowView.findViewById(R.id.textViewPhoneNum);
         ImageView ivGender=rowView.findViewById(R.id.imageViewGender);
+        ImageView imgCode=rowView.findViewById(R.id.imgCode);
 
         Contact currentItem =contactList.get(position);
         tvName.setText(currentItem.getName());
         tvCode.setText("+"+currentItem.getCountryCode());
         tvNum.setText(currentItem.getPhoneNum()+"");
+
+        String imageUrl="https://static.vecteezy.com/system/resources/previews/004/712/302/original/singapore-3d-rounded-national-flag-button-icon-vector.jpg";
+        Picasso.with(parent_context).load(imageUrl).into(imgCode);
         if(currentItem.getGender()=='F')
-        {   ivGender.setImageResource(R.drawable.female);}
+        {  String imageUrlF="https://cdn1.vectorstock.com/i/1000x1000/51/05/male-profile-avatar-with-brown-hair-vector-12055105.jpg";
+            Picasso.with(parent_context).load(imageUrlF).into(ivGender);}
         else
-        { ivGender.setImageResource(R.drawable.male);}
+        { String imageUrlM="https://cdn2.vectorstock.com/i/1000x1000/54/41/young-and-elegant-woman-avatar-profile-vector-9685441.jpg";
+            Picasso.with(parent_context).load(imageUrlM).into(ivGender);}
 
 
         return rowView;
--- app/src/main/java/sg/edu/rp/c346/id20028056/democustomcontactlist/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20028056/democustomcontactlist/MainActivity.java
@@ -3,7 +3,10 @@ package sg.edu.rp.c346.id20028056.democustomcontactlist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.ListView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
@@ -12,14 +15,20 @@ public class MainActivity extends AppCompatActivity {
     ListView lvContact;
     ArrayList<Contact> alContactList;
     CustomAdapter caContact;
+    ImageView imgvLogo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        imgvLogo=findViewById(R.id.imgViewLogo);
         lvContact=findViewById(R.id.listViewContacts);
         alContactList=new ArrayList<>();
+
+        String imageUrl="https://www.mansworldindia.com/wp-content/uploads/2020/04/harry-poruduy8re.jpg";
+        Picasso.with(this).load(imageUrl).into(imgvLogo);
+
         Contact item1= new Contact("Mary", 65, 1234567, 'F');
         Contact item2= new Contact("Ken", 65, 7654321, 'M');
         alContactList.add(item1);
